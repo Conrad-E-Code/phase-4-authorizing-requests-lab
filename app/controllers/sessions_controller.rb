5578cd5a20a6d1ad50d36879b3d74f859d0df89f
Conrad Etherington <conrad.etherington@gmail.com>
2023-01-01 16:37:06 -0700

    commit tree 07d32351a4d1bcbb641a00454a8fa233f81cb227

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     session[:user_id] = user.id
     render json: user
+
+    # if user&.authenticate params[:password]
+    #   session[:user_id] = user.id
+    #   byebug
+    #   render json: user, status: :created
+    #   else
+    #   render json: {error: {login: "Invalid username or password"}}, status: :unauthorized
+    #   end
   end
 
   def destroy
